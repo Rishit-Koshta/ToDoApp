ef5c134e323e306ff48bdca44f479d724305baea
Rishit-Koshta <koshtarishit@gmail.com>
2026-04-20 21:50:22 +0530

    Delete TodoApp directory

--- TodoApp/src/main/java/com/rishit/TodoApp/repositories/TodoRepositories.java
+++
@@ -1,8 +0,0 @@
-package com.rishit.TodoApp.repositories;
-
-import com.rishit.TodoApp.entity.TodoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepositories extends JpaRepository<TodoEntity,Long> {
-
-}
